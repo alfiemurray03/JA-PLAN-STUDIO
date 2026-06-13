0a64626b00cb3fb126a2112ff1584f68d7e8b0d3
alfiemurray1506 <alfieholywoodmurray@jagroupservices.co.uk>
2026-06-13 06:33:17 +0100

    Remove extra static site files and keep page only

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-
-const setCurrentYear = () => {
-  const yearNode = document.getElementById('year');
-  if (yearNode) yearNode.textContent = new Date().getFullYear();
-};
-
-setCurrentYear();
